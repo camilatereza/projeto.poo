1fe85f22194b7dc83798292d79b263f869078b01
Camila Tereza <camila.tereza@dce.ufpb.br>
2019-08-19 16:45:13 -0300

    Implementação das funções basicas

--- src/ufpb/poo/livraria/Library.java
+++ src/ufpb/poo/livraria/Library.java
@@ -3,10 +3,30 @@ package ufpb.poo.livraria;
 import java.util.ArrayList;
 
 public interface Library {
-	
-	public void cadastrarFuncionario();
+
+	public void cadastrarFuncionario(Funcionario fun);
 
 	public ArrayList<Livro> listarArcevo();
 
 	public boolean verificarLogin();
+
+	public void efetuarEmprestimo(Livro book, Usuario cliente);
+
+	public void efetuarDevolucao(Livro book, Usuario cliente);
+
+	public void cadastrarLivro(Livro book);
+
+	public Livro alterarLivro(Livro book);
+
+	public boolean excluirLivro(Livro book);
+
+	public Usuario consultarUsuario(Usuario cliente);
+
+	public void cadastrarUsuario(Usuario cliente);
+
+	public Usuario alterarUsuario(Usuario cliente);
+
+	public boolean excluirUsuario(Usuario Cliente);
+
+	public Livro buscaLivro(Livro book);
 }
--- src/ufpb/poo/livraria/SistemaLibrary.java
+++ src/ufpb/poo/livraria/SistemaLibrary.java
@@ -2,11 +2,109 @@ package ufpb.poo.livraria;
 
 import java.util.ArrayList;
 
-public class SistemaLibrary {
-	public static void main(String[] args) {
-		
-		ArrayList<Livro> listaLivro;
-		ArrayList<Funcionario> listaFuncionario;
-		ArrayList<Usuario> listaUsuario;
+public class SistemaLibrary implements Library {
+
+	ArrayList<Livro> listaLivro;
+	ArrayList<Funcionario> listaFuncionario;
+	ArrayList<Usuario> listaUsuario;
+
+	public void efetuarEmprestimo(Livro book, Usuario cliente) {
+		cliente.setLivroLocado(book);
+		book.setDisponivel(false);
+	}
+
+	public void efetuarDevolucao(Livro book, Usuario cliente) {
+		cliente.setLivroLocado(book);
+		book.setDisponivel(true);
+	}
+
+	public void cadastrarLivro(Livro book) {
+		listaLivro.add(book);
+	}
+
+	public Livro alterarLivro(Livro book) {
+		return this.buscaLivro(book);
+	}
+
+	public Livro buscaLivro(Livro book) {
+		for (Livro l : listaLivro) {
+			if (l.getCodigo().equals(book.getCodigo())) {
+				return l;
+			}
+		}
+		return null;
+	}
+
+	public boolean excluirLivro(Livro book) {
+		boolean tmp = false;
+		for (Livro l : listaLivro) {
+			if (l.getCodigo().equals(book.getCodigo())) {
+				tmp = true;
+				listaLivro.remove(book);
+			}
+		}
+		return tmp;
+	}
+
+	public Usuario consultarUsuario(Usuario cliente) {
+		for (Usuario u : listaUsuario) {
+			if (u.getCpf().equals(cliente.getCpf())) {
+				return u;
+			}
+		}
+		return null;
+	}
+
+	public void cadastrarUsuario(Usuario cliente) {
+		listaUsuario.add(cliente);
+	}
+
+	public Usuario alterarUsuario(Usuario cliente) {
+		return this.consultarUsuario(cliente);
+	}
+
+	public boolean excluirUsuario(Usuario cliente) {
+		boolean tmp = false;
+		for (Usuario c : listaUsuario) {
+			if (c.getCpf().equals(cliente.getCpf())) {
+				tmp = true;
+				listaUsuario.remove(cliente);
+			}
+		}
+		return tmp;
+	}
+
+	@Override
+	public void cadastrarFuncionario(Funcionario fun) {
+		this.listaFuncionario.add(fun);
+	}
+
+	@Override
+	public ArrayList<Livro> listarArcevo() {
+		return this.listaLivro;
+	}
+
+	@Override
+	public boolean verificarLogin() {
+		return false;
+	}
+
+	public ArrayList<Usuario> buscaUsu(String nome) {
+		ArrayList<Usuario> usuariosEncontrados = new ArrayList<Usuario>();
+		for (Usuario c : listaUsuario) {
+			if (c.getNome().equals(nome)) {
+				usuariosEncontrados.add(c);
+			}
+		}
+		return usuariosEncontrados;
+	}
+
+	public Usuario buscaCpf(String cpf) {
+		for (Usuario c : listaUsuario) {
+			if (c.getCpf().equals(cpf)) {
+				return c;
+			}
+		}
+		return null;
 	}
 }
